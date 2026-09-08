df7ce174bd00791b05f6e0def295ad05702568ee
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-08 17:51:55 +0530

    Expose robust trend methods

--- src/climatevar/trends/__init__.py
+++ src/climatevar/trends/__init__.py
@@ -1,5 +1,5 @@
 """Trend and monotonic-change diagnostics."""
 
-from .trend import mann_kendall, sens_slope
+from .trend import mann_kendall, modified_mann_kendall, sens_slope, trend_free_prewhitening
 
-__all__ = ["mann_kendall", "sens_slope"]
+__all__ = ["mann_kendall", "modified_mann_kendall", "sens_slope", "trend_free_prewhitening"]
