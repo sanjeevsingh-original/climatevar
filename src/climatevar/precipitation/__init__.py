f62589daca8d2981bc4a58958b9c39d3db018e8c
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-08 12:43:51 +0530

    Add precipitation package

---
+++ src/climatevar/precipitation/__init__.py
@@ -0,0 +1,25 @@
+"""Precipitation indices and hydroclimate diagnostics."""
+
+from .indices import (
+    cdd,
+    cwd,
+    prcptot,
+    r10mm,
+    r20mm,
+    r95p,
+    r99p,
+    rx1day,
+    rx5day,
+)
+
+__all__ = [
+    "cdd",
+    "cwd",
+    "prcptot",
+    "r10mm",
+    "r20mm",
+    "r95p",
+    "r99p",
+    "rx1day",
+    "rx5day",
+]
