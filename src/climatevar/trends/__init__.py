acbc97f057c415c8cc73979637d15be699cae9c8
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-08 17:54:02 +0530

    Export seasonal and spatial trend diagnostics

--- src/climatevar/trends/__init__.py
+++ src/climatevar/trends/__init__.py
@@ -1,5 +1,9 @@
 """Trend and monotonic-change diagnostics."""
 
+from .seasonal import fdr_mask, seasonal_mann_kendall, seasonal_sen_slope, seasonal_series, spatial_trend
 from .trend import mann_kendall, modified_mann_kendall, sens_slope, trend_free_prewhitening
 
-__all__ = ["mann_kendall", "modified_mann_kendall", "sens_slope", "trend_free_prewhitening"]
+__all__ = [
+    "mann_kendall", "modified_mann_kendall", "sens_slope", "trend_free_prewhitening",
+    "seasonal_series", "seasonal_mann_kendall", "seasonal_sen_slope", "fdr_mask", "spatial_trend",
+]
