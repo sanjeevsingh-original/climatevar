08d72f429e20219178d445f6e7a4e82126898573
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-08 13:10:56 +0530

    Expose dataset normalization utilities

--- src/climatevar/io/__init__.py
+++ src/climatevar/io/__init__.py
@@ -1,5 +1,12 @@
 """Input/output helpers for common climate datasets."""
 
 from .dataset import open_dataset
+from .normalize import find_variable, normalize_coords, normalize_dataset, standardize_variables
 
-__all__ = ["open_dataset"]
+__all__ = [
+    "find_variable",
+    "normalize_coords",
+    "normalize_dataset",
+    "open_dataset",
+    "standardize_variables",
+]
