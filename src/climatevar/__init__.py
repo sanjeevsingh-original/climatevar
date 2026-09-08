b618d2032eeed77de66b51da2511737cb9f566e2
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-08 13:04:00 +0530

    Expose core climatevar API

--- src/climatevar/__init__.py
+++ src/climatevar/__init__.py
@@ -1,5 +1,19 @@
 """Climate variability and atmospheric science tools."""
 
 from ._version import __version__
+from .climatology import anomaly, climatology
+from .drought import spi_like
+from .quality import annual_valid_fraction, require_completeness, valid_fraction
+from .seasonal import monsoon_total, seasonal_mean
 
-__all__ = ["__version__"]
+__all__ = [
+    "__version__",
+    "anomaly",
+    "annual_valid_fraction",
+    "climatology",
+    "monsoon_total",
+    "require_completeness",
+    "seasonal_mean",
+    "spi_like",
+    "valid_fraction",
+]
