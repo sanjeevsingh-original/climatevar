316ad3011cadd9b5952ebef60e944123ec97b5dc
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 11:47:10 +0530

    Export rainfall error-tagging split helper

--- src/climatevar/error_tagging/__init__.py
+++ src/climatevar/error_tagging/__init__.py
@@ -1,10 +1,10 @@
 """Rainfall model-error tagging and leakage-safe ML dataset utilities."""
 from .labels import error_class, intensity_class
 from .features import build_error_features, to_ml_table
-from .split import event_group_split, spatial_block_split, temporal_split, validate_no_overlap
+from .split import assign_split, event_group_split, spatial_block_split, temporal_split, validate_no_overlap
 
 __all__ = [
     "error_class", "intensity_class", "build_error_features", "to_ml_table",
     "temporal_split", "event_group_split", "spatial_block_split",
-    "validate_no_overlap",
+    "validate_no_overlap", "assign_split",
 ]
