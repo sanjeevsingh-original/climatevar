c40d17458dad1ad8b8240b61687030fa61890f7e
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 12:46:21 +0530

    Export publication report helpers

--- src/climatevar/verification/__init__.py
+++ src/climatevar/verification/__init__.py
@@ -3,10 +3,15 @@ from .compare import align_period, common_grid, compare_products, evaluate_produ
 from .datasets import PRESETS, ProductSpec, load_precipitation
 from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, ExperimentResult, REGION_BOUNDS, run_experiment, save_experiment
 from .odisha import ODISHA_BBOX, odisha_experiment, subset_odisha, validate_input_paths
+from .plots import plot_scorecard, plot_spatial_metric
+from .report import generate_publication_report, intensity_distribution, manuscript_summary, ranking_heatmap, seasonal_heatmap, spatial_metric_panels, uncertainty_plot
 
 __all__ = [
     "align_period", "common_grid", "compare_products", "evaluate_product", "rank_products",
     "PRESETS", "ProductSpec", "load_precipitation",
     "DEFAULT_INTENSITIES", "DEFAULT_SEASONS", "REGION_BOUNDS", "ExperimentConfig", "ExperimentResult",
     "run_experiment", "save_experiment", "ODISHA_BBOX", "odisha_experiment", "subset_odisha", "validate_input_paths",
+    "plot_spatial_metric", "plot_scorecard",
+    "spatial_metric_panels", "seasonal_heatmap", "intensity_distribution", "uncertainty_plot", "ranking_heatmap",
+    "manuscript_summary", "generate_publication_report",
 ]
