2bfdb4a707637d03b4e90b35791a0b4106b3f792
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 12:47:49 +0530

    Export error distribution report helper

--- src/climatevar/verification/__init__.py
+++ src/climatevar/verification/__init__.py
@@ -4,7 +4,7 @@ from .datasets import PRESETS, ProductSpec, load_precipitation
 from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, ExperimentResult, REGION_BOUNDS, run_experiment, save_experiment
 from .odisha import ODISHA_BBOX, odisha_experiment, subset_odisha, validate_input_paths
 from .plots import plot_scorecard, plot_spatial_metric
-from .report import generate_publication_report, intensity_distribution, manuscript_summary, ranking_heatmap, seasonal_heatmap, spatial_metric_panels, uncertainty_plot
+from .report import error_feature_distribution, generate_publication_report, intensity_distribution, manuscript_summary, ranking_heatmap, seasonal_heatmap, spatial_metric_panels, uncertainty_plot
 
 __all__ = [
     "align_period", "common_grid", "compare_products", "evaluate_product", "rank_products",
@@ -12,6 +12,6 @@ __all__ = [
     "DEFAULT_INTENSITIES", "DEFAULT_SEASONS", "REGION_BOUNDS", "ExperimentConfig", "ExperimentResult",
     "run_experiment", "save_experiment", "ODISHA_BBOX", "odisha_experiment", "subset_odisha", "validate_input_paths",
     "plot_spatial_metric", "plot_scorecard",
-    "spatial_metric_panels", "seasonal_heatmap", "intensity_distribution", "uncertainty_plot", "ranking_heatmap",
-    "manuscript_summary", "generate_publication_report",
+    "spatial_metric_panels", "seasonal_heatmap", "intensity_distribution", "error_feature_distribution",
+    "uncertainty_plot", "ranking_heatmap", "manuscript_summary", "generate_publication_report",
 ]
