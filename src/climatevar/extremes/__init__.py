0e1ba0b547d7ac599a44a7face1322f40343791b
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 10:07:52 +0530

    feat: expose POT analysis API

--- src/climatevar/extremes/__init__.py
+++ src/climatevar/extremes/__init__.py
@@ -1,5 +1,17 @@
 """Extreme-value analysis utilities."""
 
 from .gev import gev_fit, gev_return_level, gev_return_level_ci
+from .pot import (
+    decluster_exceedances,
+    gpd_fit,
+    pot_exceedances,
+    pot_return_level,
+    pot_return_level_ci,
+    threshold_diagnostics,
+)
 
-__all__ = ["gev_fit", "gev_return_level", "gev_return_level_ci"]
+__all__ = [
+    "gev_fit", "gev_return_level", "gev_return_level_ci",
+    "gpd_fit", "pot_exceedances", "decluster_exceedances",
+    "threshold_diagnostics", "pot_return_level", "pot_return_level_ci",
+]
