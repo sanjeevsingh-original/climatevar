54755619acb512939f493d5f7d2826c6de2115bc
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 10:16:08 +0530

    Export POT diagnostics and threshold sensitivity

--- src/climatevar/extremes/__init__.py
+++ src/climatevar/extremes/__init__.py
@@ -4,14 +4,17 @@ from .gev import gev_fit, gev_return_level, gev_return_level_ci
 from .pot import (
     decluster_exceedances,
     gpd_fit,
+    gpd_goodness_of_fit,
     pot_exceedances,
     pot_return_level,
     pot_return_level_ci,
+    pot_threshold_sensitivity,
     threshold_diagnostics,
 )
 
 __all__ = [
     "gev_fit", "gev_return_level", "gev_return_level_ci",
-    "gpd_fit", "pot_exceedances", "decluster_exceedances",
-    "threshold_diagnostics", "pot_return_level", "pot_return_level_ci",
+    "gpd_fit", "gpd_goodness_of_fit", "pot_exceedances", "decluster_exceedances",
+    "threshold_diagnostics", "pot_threshold_sensitivity", "pot_return_level",
+    "pot_return_level_ci",
 ]
