d1c8d31f4b03b435d0cde3eda8b7f241b9df9482
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 11:57:36 +0530

    Expose comparative precipitation verification API

---
+++ src/climatevar/verification/__init__.py
@@ -0,0 +1,4 @@
+"""Precipitation verification and comparative evaluation utilities."""
+from .compare import align_period, common_grid, compare_products, evaluate_product, rank_products
+
+__all__ = ["align_period", "common_grid", "compare_products", "evaluate_product", "rank_products"]
