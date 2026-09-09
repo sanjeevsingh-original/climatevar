ec97d41041ed1c0cb89e306213aee63c4fd4c26a
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 12:10:24 +0530

    Export experiment and dataset adapters

--- src/climatevar/verification/__init__.py
+++ src/climatevar/verification/__init__.py
@@ -1,4 +1,11 @@
 """Precipitation verification and comparative evaluation utilities."""
 from .compare import align_period, common_grid, compare_products, evaluate_product, rank_products
+from .datasets import PRESETS, ProductSpec, load_precipitation
+from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, ExperimentResult, run_experiment, save_experiment
 
-__all__ = ["align_period", "common_grid", "compare_products", "evaluate_product", "rank_products"]
+__all__ = [
+    "align_period", "common_grid", "compare_products", "evaluate_product", "rank_products",
+    "PRESETS", "ProductSpec", "load_precipitation",
+    "DEFAULT_INTENSITIES", "DEFAULT_SEASONS", "ExperimentConfig", "ExperimentResult",
+    "run_experiment", "save_experiment",
+]
