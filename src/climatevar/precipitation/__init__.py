b39861bf55cee3de677fbf9aa263c5e98d5272ec
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 09:44:27 +0530

    Export ETCCDI precipitation diagnostics

--- src/climatevar/precipitation/__init__.py
+++ src/climatevar/precipitation/__init__.py
@@ -1,32 +1,14 @@
 """Precipitation normalization, indices, and hydroclimate diagnostics."""
 
 from .indices import (
-    cdd,
-    cwd,
-    prcptot,
-    r10mm,
-    r20mm,
-    r95p,
-    r99p,
-    rx1day,
-    rx5day,
+    cdd, cwd, prcptot, r10mm, r20mm, r95p, r99p, r95p_fraction, r99p_fraction,
+    rx1day, rx5day, sdii, wet_day_count,
 )
 from .normalize import daily_amount, interval_amount_mm, normalize_precipitation
 from .wrf import wrf_precipitation_amount, wrf_total_precipitation
 
 __all__ = [
-    "cdd",
-    "cwd",
-    "daily_amount",
-    "interval_amount_mm",
-    "normalize_precipitation",
-    "prcptot",
-    "r10mm",
-    "r20mm",
-    "r95p",
-    "r99p",
-    "rx1day",
-    "rx5day",
-    "wrf_precipitation_amount",
-    "wrf_total_precipitation",
+    "cdd", "cwd", "daily_amount", "interval_amount_mm", "normalize_precipitation",
+    "prcptot", "r10mm", "r20mm", "r95p", "r99p", "r95p_fraction", "r99p_fraction",
+    "rx1day", "rx5day", "sdii", "wet_day_count", "wrf_precipitation_amount", "wrf_total_precipitation",
 ]
