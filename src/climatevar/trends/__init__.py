2967e5a0f0cff4482d8d3d486ba8a67720ecc24d
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 09:32:33 +0530

    Expose DFA long-memory diagnostic

--- src/climatevar/trends/__init__.py
+++ src/climatevar/trends/__init__.py
@@ -1,11 +1,13 @@
 """Trend and monotonic-change diagnostics."""
 
 from .field import field_significance, regional_mean, regional_slope_ci, regional_trend
+from .long_memory import dfa_hurst
 from .seasonal import fdr_mask, seasonal_mann_kendall, seasonal_sen_slope, seasonal_series, spatial_trend
 from .trend import mann_kendall, modified_mann_kendall, sens_slope, sens_slope_ci, trend_free_prewhitening
 
 __all__ = [
     "mann_kendall", "modified_mann_kendall", "sens_slope", "sens_slope_ci", "trend_free_prewhitening",
+    "dfa_hurst",
     "seasonal_series", "seasonal_mann_kendall", "seasonal_sen_slope", "fdr_mask", "spatial_trend",
     "regional_mean", "regional_trend", "regional_slope_ci", "field_significance",
 ]
