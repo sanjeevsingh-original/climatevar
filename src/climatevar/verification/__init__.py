dcffd0c2d31a2ced8ff21bc74c39c1c4c313fd8d
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 12:17:21 +0530

    Export Odisha experiment and plotting helpers

--- src/climatevar/verification/__init__.py
+++ src/climatevar/verification/__init__.py
@@ -1,11 +1,12 @@
 """Precipitation verification and comparative evaluation utilities."""
 from .compare import align_period, common_grid, compare_products, evaluate_product, rank_products
 from .datasets import PRESETS, ProductSpec, load_precipitation
-from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, ExperimentResult, run_experiment, save_experiment
+from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, ExperimentResult, REGION_BOUNDS, run_experiment, save_experiment
+from .odisha import ODISHA_BBOX, odisha_experiment, subset_odisha, validate_input_paths
 
 __all__ = [
     "align_period", "common_grid", "compare_products", "evaluate_product", "rank_products",
     "PRESETS", "ProductSpec", "load_precipitation",
-    "DEFAULT_INTENSITIES", "DEFAULT_SEASONS", "ExperimentConfig", "ExperimentResult",
-    "run_experiment", "save_experiment",
+    "DEFAULT_INTENSITIES", "DEFAULT_SEASONS", "REGION_BOUNDS", "ExperimentConfig", "ExperimentResult",
+    "run_experiment", "save_experiment", "ODISHA_BBOX", "odisha_experiment", "subset_odisha", "validate_input_paths",
 ]
