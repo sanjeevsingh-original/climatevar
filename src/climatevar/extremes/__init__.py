e2ecdc5882b8795100b8be5f032d2bb36573869e
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-09 10:37:12 +0530

    Export advanced EVT inference and diagnostics

--- src/climatevar/extremes/__init__.py
+++ src/climatevar/extremes/__init__.py
@@ -6,4 +6,18 @@ from .pot import (
     pot_return_level_ci, pot_threshold_sensitivity, pot_threshold_uncertainty,
     threshold_diagnostics,
 )
-__all__=["gev_fit","gev_return_level","gev_return_level_ci","gpd_fit","gpd_fit_nonstationary","gpd_goodness_of_fit","gpd_goodness_of_fit_bootstrap","pot_exceedances","decluster_exceedances","threshold_diagnostics","pot_threshold_sensitivity","pot_threshold_uncertainty","pot_return_level","pot_return_level_ci"]
+from .advanced import (
+    gpd_model_comparison, gpd_model_comparison_bootstrap,
+    gpd_nonstationary_ci, pot_threshold_bootstrap,
+)
+from .diagnostics import plot_gpd_qq, plot_mean_excess, plot_threshold_stability, pot_diagnostic_report
+
+__all__ = [
+    "gev_fit", "gev_return_level", "gev_return_level_ci", "gpd_fit", "gpd_fit_nonstationary",
+    "gpd_goodness_of_fit", "gpd_goodness_of_fit_bootstrap", "gpd_model_comparison",
+    "gpd_model_comparison_bootstrap", "gpd_nonstationary_ci", "pot_exceedances",
+    "decluster_exceedances", "threshold_diagnostics", "pot_threshold_sensitivity",
+    "pot_threshold_uncertainty", "pot_threshold_bootstrap", "pot_return_level",
+    "pot_return_level_ci", "plot_threshold_stability", "plot_mean_excess", "plot_gpd_qq",
+    "pot_diagnostic_report",
+]
