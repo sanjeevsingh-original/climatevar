de1328f49d6ad621855ba7f40ca74c8f90642f53
Sanjeev Singh <sanjeev.11703464@gmail.com>
2026-09-10 16:51:32 +0530

    Export cartographic publication helpers

--- src/climatevar/verification/__init__.py
+++ src/climatevar/verification/__init__.py
@@ -5,6 +5,7 @@ from .experiment import DEFAULT_INTENSITIES, DEFAULT_SEASONS, ExperimentConfig, 
 from .odisha import ODISHA_BBOX, odisha_experiment, subset_odisha, validate_input_paths
 from .plots import plot_scorecard, plot_spatial_metric
 from .report import error_feature_distribution, generate_publication_report, intensity_distribution, manuscript_summary, ranking_heatmap, seasonal_heatmap, spatial_metric_panels, uncertainty_plot
+from .cartography import export_publication_figure, publication_map
 
 __all__ = [
     "align_period", "common_grid", "compare_products", "evaluate_product", "rank_products",
@@ -14,4 +15,5 @@ __all__ = [
     "plot_spatial_metric", "plot_scorecard",
     "spatial_metric_panels", "seasonal_heatmap", "intensity_distribution", "error_feature_distribution",
     "uncertainty_plot", "ranking_heatmap", "manuscript_summary", "generate_publication_report",
+    "publication_map", "export_publication_figure",
 ]
